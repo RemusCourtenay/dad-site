ada6eeb9b4981ca11cfc323c88181d6b75bd2d12
Remus Courtenay <remuscourtenay@gmail.com>
2020-11-03 18:37:57 +1300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Remus Courtenay - rcou199
+ * @since 3/11/2020
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
